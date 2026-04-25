f5b0b4a9e00a39d35407bb42cf7f7ef315bbf45f
Ahmad Hariri <ahmadhrr22@gmail.com>
2026-04-25 20:21:05 +0700

    change frontend domain

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://matverify-1oyb.vercel.app' // URL Frontend Vercel kamu
+    'https://matverify.vercel.app' // URL Frontend Vercel kamu
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
