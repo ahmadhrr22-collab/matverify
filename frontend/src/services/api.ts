2eeaf2cc959cf95035800cdeb4123cb71bd12382
Ahmad Hariri <ahmadhrr22@gmail.com>
2026-04-25 20:30:14 +0700

    change frontend domain 2

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ 
-  baseURL: 'https://matverify.vercel.app/api',
+  baseURL: 'https://matverify-3orgf00wd-ahmadhrr22-collabs-projects.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
